bd0a7616589352a02921685b4836d297079b9ef6
DukeRupert <loganwilliamsse@gmail.com>
2020-09-16 22:26:20 -0700

    Edited Elders. Colin vice-chairman, removed Ken and Dale.

--- src/routes/staff/index.svelte
+++ src/routes/staff/index.svelte
@@ -1,188 +1,172 @@
 <script>
-	import StaffCard from '../../components/StaffCard.svelte';
+  import StaffCard from "../../components/StaffCard.svelte";
 
-	const alecPaul = {
-		src: '/staff/Alec400px.png', 
-		route: '/staff/alecPaul',
-		name: 'Alec Paul',
-		title: 'Pastor - Chairman',
-		email: 'pastor@theridgebiblechurch.com'
-	};
+  const alecPaul = {
+    src: "/staff/Alec400px.png",
+    route: "/staff/alecPaul",
+    name: "Alec Paul",
+    title: "Pastor - Chairman",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const dennisRiksen = {
-		src: '/staff/Dennis400px.png', 
-		name: 'Dennis Riksen',
-		title: 'Vice-Chairman',
-		//email: 'pastor@theridgebiblechurch.com'
-	};
+  const dennisRiksen = {
+    src: "/staff/Dennis400px.png",
+    name: "Dennis Riksen",
+    title: "Elder",
+    //email: 'pastor@theridgebiblechurch.com'
+  };
 
-	const mattArmstrong = {
-		src: '/staff/Matt400px.png', 
-		name: 'Matt Armstrong',
-		title: 'Secretary',
-		email: 'themsarmstrongs@live.com'
-	};
+  const mattArmstrong = {
+    src: "/staff/Matt400px.png",
+    name: "Matt Armstrong",
+    title: "Secretary",
+    email: "themsarmstrongs@live.com",
+  };
 
-	const kenKlubberud = {
-		//src: '/alec.jpg', 
-		name: 'Ken Klubberud',
-		title: 'Financial Officer',
-		//email: 'pastor@theridgebiblechurch.com'
-	};
+  const gregBennet = {
+    src: "/staff/Greg400px.png",
+    name: "Greg Bennett",
+    title: "Elder",
+    //email: 'pastor@theridgebiblechurch.com'
+  };
 
-	const gregBennet = {
-		src: '/staff/Greg400px.png', 
-		name: 'Greg Bennett',
-		title: 'Elder',
-		//email: 'pastor@theridgebiblechurch.com'
-	};
+  const colinKertson = {
+    //src: '/alec.jpg',
+    name: "Colin Kertson",
+    title: "Vice-Chairman",
+    email: "cpk2234@aol.com",
+  };
 
-	const colinKertson = {
-		//src: '/alec.jpg', 
-		name: 'Colin Kertson',
-		title: 'Elder',
-		email: 'cpk2234@aol.com'
-	};
+  const sueArmstrong = {
+    //src: '/alec.jpg',
+    name: "Sue Armstrong",
+    title: "Childrens's Ministry Director",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const daleForrester = {
-		//src: '/alec.jpg', 
-		name: 'Dale Forrester',
-		title: 'Elder',
-		//email: 'pastor@theridgebiblechurch.com'
-	};
+  const reamenWilliams = {
+    //src: '/alec.jpg',
+    name: "Reamen Williams",
+    title: "Youth Ministry Director",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const sueArmstrong = {
-		//src: '/alec.jpg', 
-		name: 'Sue Armstrong',
-		title: 'Childrens\'s Ministry Director',
-		email: 'pastor@theridgebiblechurch.com'
-	};
+  const debbieNewton = {
+    //src: '/alec.jpg',
+    name: "Debbie Newton",
+    title: "Church Secretary",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const reamenWilliams = {
-		//src: '/alec.jpg', 
-		name: 'Reamen Williams',
-		title: 'Youth Ministry Director',
-		email: 'pastor@theridgebiblechurch.com'
-	};
+  const edKyllonen = {
+    //src: '/alec.jpg',
+    name: "Ed Kyllonen",
+    title: "Deacon",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const debbieNewton = {
-		//src: '/alec.jpg', 
-		name: 'Debbie Newton',
-		title: 'Church Secretary',
-		email: 'pastor@theridgebiblechurch.com'
-	};
+  const jessKady = {
+    //src: '/alec.jpg',
+    name: "Jess Kady",
+    title: "Deacon",
+    email: "pastor@theridgebiblechurch.com",
+  };
 
-	const edKyllonen = {
-		//src: '/alec.jpg', 
-		name: 'Ed Kyllonen',
-		title: 'Deacon',
-		email: 'pastor@theridgebiblechurch.com'
-	};
-
-	const jessKady = {
-		//src: '/alec.jpg', 
-		name: 'Jess Kady',
-		title: 'Deacon',
-		email: 'pastor@theridgebiblechurch.com'
-	};
-
-	const mattCrowell = {
-		//src: '/alec.jpg', 
-		name: 'Matt Crowell',
-		title: 'Deacon',
-		email: 'pastor@theridgebiblechurch.com'
-	};
-
+  const mattCrowell = {
+    //src: '/alec.jpg',
+    name: "Matt Crowell",
+    title: "Deacon",
+    email: "pastor@theridgebiblechurch.com",
+  };
 </script>
 
 <style>
-	div {
-		padding: 0 0.5em;
-		display: flex;
-		flex-direction: column;
-		flex-wrap: wrap;
-	}
-	
-	hr {
-		width: 100%;
-		margin-left: 0;
-		margin: 0;
-	}
+  div {
+    padding: 0 0.5em;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+  }
 
-	h1 {
-		font-weight: bold;
-	}
+  hr {
+    width: 100%;
+    margin-left: 0;
+    margin: 0;
+  }
 
-	h2 {
-		margin-top: 2em;
-		font-size: 1.5em;
-		font-weight: bold;
-	}
+  h1 {
+    font-weight: bold;
+  }
 
-	p {
-		font-style: italic;
-	}
+  h2 {
+    margin-top: 2em;
+    font-size: 1.5em;
+    font-weight: bold;
+  }
 
-	/*Larger Screen*/
-	@media only screen and (min-width: 655px) {
+  p {
+    font-style: italic;
+  }
 
-	div {
-		padding: 0 2em;
-	}
+  /*Larger Screen*/
+  @media only screen and (min-width: 655px) {
+    div {
+      padding: 0 2em;
+    }
 
-	.wrapper {
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: flex-start;
-	}
+    .wrapper {
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: flex-start;
+    }
 
-	h1 {
-		font-size: 4em;
-	}
-}
-
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Staff</title>
+  <title>Staff</title>
 </svelte:head>
 
 <div>
-	<h1>Our Leadership</h1>
+  <h1>Our Leadership</h1>
 
-	<p>It shall not be so among you. But whoever would be great among you must be your servant... Matthew 20:26</p>
-	
-	<h2>Board of Elders</h2>
-	<hr>
-	<div class='wrapper'>
-		<StaffCard {...alecPaul}/>
-		<StaffCard {...dennisRiksen}/>
-		<StaffCard {...mattArmstrong}/>
-		<StaffCard {...colinKertson}/>
-		<StaffCard {...gregBennet}/>
-		<StaffCard {...kenKlubberud}/>
-		<StaffCard {...daleForrester}/>
-	</div>
+  <p>
+    It shall not be so among you. But whoever would be great among you must be
+    your servant... Matthew 20:26
+  </p>
 
-	<h2>Deacons</h2>
-	<hr>
-	<div class='wrapper'>
-		<StaffCard {...edKyllonen}/>
-		<StaffCard {...jessKady}/>
-		<StaffCard {...mattCrowell}/>
-	</div>
+  <h2>Board of Elders</h2>
+  <hr />
+  <div class="wrapper">
+    <StaffCard {...alecPaul} />
+    <StaffCard {...colinKertson} />
+    <StaffCard {...dennisRiksen} />
+    <StaffCard {...mattArmstrong} />
+    <StaffCard {...gregBennet} />
+  </div>
 
-	<h2>Directors</h2>
-	<hr>
-	<div class='wrapper'>
-		<StaffCard {...sueArmstrong}/>
-		<StaffCard {...reamenWilliams}/>
-	</div>
+  <h2>Deacons</h2>
+  <hr />
+  <div class="wrapper">
+    <StaffCard {...edKyllonen} />
+    <StaffCard {...jessKady} />
+    <StaffCard {...mattCrowell} />
+  </div>
 
-	<h2>Support</h2>
-	<hr>
-	<div class='wrapper'>
-		<StaffCard {...debbieNewton}/>
-	</div>
+  <h2>Directors</h2>
+  <hr />
+  <div class="wrapper">
+    <StaffCard {...sueArmstrong} />
+    <StaffCard {...reamenWilliams} />
+  </div>
+
+  <h2>Support</h2>
+  <hr />
+  <div class="wrapper">
+    <StaffCard {...debbieNewton} />
+  </div>
 
 </div>
