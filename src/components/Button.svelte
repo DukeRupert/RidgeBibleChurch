efe56bac8019b6e0bd316a982eb15825db4dd442
DukeRupert <loganwilliamsse@gmail.com>
2020-09-16 22:46:28 -0700

    Added contact-us page. Imported Button component. Still need to customize both.

---
+++ src/components/Button.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let label = "FixMe";
+  export let route = "";
+  export let center = false;
+</script>
+
+<style>
+  button {
+    background-color: var(--buttonBackground);
+    border: none;
+    border-radius: 10px;
+    color: white;
+    padding: 0.5em 1em;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1.5em;
+    transition: all 300ms;
+    align-self: center;
+    margin-top: 40px;
+    margin-bottom: 20px;
+  }
+
+  button:hover {
+    background-color: var(--buttonBackgroundHover);
+    color: var(--pink);
+  }
+
+  .center {
+    align-self: center;
+  }
+</style>
+
+<a href={route} class:center> <button> {label} </button> </a>
---
+++ src/routes/contact-us.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Button from "../components/Button.svelte";
+</script>
+
+<section>
+  <h1>CONTACT US</h1>
+  <form
+    name="contact"
+    method="POST"
+    data-netlify="true"
+    netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+    <div hidden aria-hidden="true">
+      <label>
+        Don’t fill this out if you're human:
+        <input name="bot-field" />
+      </label>
+    </div>
+    <label>NAME - Optional</label>
+    <input type="text" name="name" />
+    <label>EMAIL - Required</label>
+    <input type="email" name="email" required />
+    <label>MESSAGE - Required</label>
+    <textarea rows="6" type="text" name="message" required />
+    <Button type="submit" label="Submit" center />
+  </form>
+</section>
