3c72519be728857dd783b2c285c0da940418adf5
DukeRupert <loganwilliamsse@gmail.com>
2020-10-30 22:58:45 -0700

    Function working on local dev. Testing on production.

--- functions/submission-created/submission-created.js
+++ functions/submission-created/submission-created.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 
-exports.handler = async (event, context) => {
+exports.handler = function(event, context, callback) {
     
     let transporter =  nodemailer.createTransport({
         service: "Postmark",
@@ -10,19 +10,23 @@ exports.handler = async (event, context) => {
             user: process.env.POSTMARK,
             pass: process.env.POSTMARK,
         }
-    })
+    });
+    console.log(event.body);
 
-    try {
+    
         transporter.sendMail({
             from: "firefly@loganwilliams.tech",
             to: "logan@firefly.llc",
             subject: "Test Email from Nodemailer",
             text: "Hello World!"
+        }, function(error, info) {
+            if (error) {
+                callback(error);
+            } else {
+                callback(null, {
+                    statusCode: 200,
+                    body: "OK"
+                });
+            }
         });
-        return {
-            statusCode: 200,
-        }
-    } catch (err) {
-        return { statusCode: 500, body:err.toString() }
-    }
 }
